c121c33ee14547c4ae8df054a611487a36fc131b
fmonegaglia <f.monegaglia@gmail.com>
2018-04-17 11:09:13 +0200

    Updated publication

--- setup.py
+++ setup.py
@@ -27,13 +27,13 @@ long_description = '\n'.join((
 
     See Publication:
     
-    --------------------------------------------------------------------------------------------------
-    Monegaglia et al.
-    "Automated extraction of meandering river morphodynamics from multitemporal remotely sensed data"
-    (under consideration for publication in "Environmental Modeling & Software")
-    --------------------------------------------------------------------------------------------------
+    --------------------------------------------------------------------------------------------------------
+    Monegaglia et al., 2018
+    "Automated extraction of meandering river morphodynamics from multitemporal remotely sensed data",
+    Environmental Modeling & Software [https://www.sciencedirect.com/science/article/pii/S1364815217309118]
+    --------------------------------------------------------------------------------------------------------
 
-    Requires: NumPy, SciPy, MatPlotLib, GDAL
+    Requires: NumPy, SciPy, MatPlotLib, Scikits-Image, GDAL
     
     '''
 ))
